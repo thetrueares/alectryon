1896006932f56e7861f7eec8c5e699b91d856af3
Iain Cambridge <iain@iain.rocks>
2026-05-24 09:44:16 +0200

    Update views with titles

--- web/src/router.js
+++ web/src/router.js
@@ -3,13 +3,25 @@ import HomeView from './Views/HomeView.vue'
 import InputsView from './Views/InputsView.vue'
 
 const routes = [
-  { path: '/', component: HomeView },
-  { path: '/inputs', component: InputsView },
+  { 
+    path: '/', 
+    component: HomeView,
+    meta: { title: 'Home - Alectryon' }
+  },
+  { 
+    path: '/inputs', 
+    component: InputsView,
+    meta: { title: 'Inputs - Alectryon' }
+  },
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+})
+
+router.afterEach((to) => {
+  document.title = to.meta.title || 'Alectryon'
 })
 
 export default router
