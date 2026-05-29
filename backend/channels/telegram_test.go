e32ea14d08242cb71e683bab26d94faf36a73606
Iain Cambridge <iain@iain.rocks>
2026-05-29 14:05:05 +0200

    Fix test

--- backend/channels/telegram_test.go
+++ backend/channels/telegram_test.go
@@ -10,58 +10,52 @@ import (
 	"go.mongodb.org/mongo-driver/v2/bson"
 )
 
-type dummyAi struct{}
-
-func (d dummyAi) Process(in engine.Input) engine.Output {
-	return engine.Output{Text: in.Text}
-}
-
 func TestStartTelegramBot_InvalidType(t *testing.T) {
-	input := entities.ChannelEntity{
+	input := &entities.ChannelEntity{
 		Type: entities.ChannelTypeSlackBot,
 	}
-	err := channels.StartTelegramBot(input, nil, nil, dummyAi{})
+	err := channels.StartTelegramBot(input, make(chan engine.InputMessage))
 	assert.Error(t, err)
 	assert.Contains(t, err.Error(), "invalid input type")
 }
 
 func TestStartTelegramBot_MissingToken(t *testing.T) {
-	input := entities.ChannelEntity{
+	input := &entities.ChannelEntity{
 		Type:    entities.ChannelTypeTelegramBot,
 		Options: map[string]any{},
 	}
-	err := channels.StartTelegramBot(input, nil, nil, dummyAi{})
+	err := channels.StartTelegramBot(input, make(chan engine.InputMessage))
 	assert.Error(t, err)
 	assert.Contains(t, err.Error(), "bot_token not found")
 }
 
 func TestStartTelegramBot_InvalidTokenFormat(t *testing.T) {
-	input := entities.ChannelEntity{
+	input := &entities.ChannelEntity{
 		Type: entities.ChannelTypeTelegramBot,
 		Options: map[string]any{
 			"bot_token": 12345,
 		},
 	}
-	err := channels.StartTelegramBot(input, nil, nil, dummyAi{})
+	err := channels.StartTelegramBot(input, make(chan engine.InputMessage))
 	assert.Error(t, err)
 	assert.Contains(t, err.Error(), "bot_token must be a string")
 }
 
 func TestStartTelegramBot_EmptyToken(t *testing.T) {
-	input := entities.ChannelEntity{
+	input := &entities.ChannelEntity{
 		Type: entities.ChannelTypeTelegramBot,
 		Options: map[string]any{
 			"bot_token": "",
 		},
 	}
-	err := channels.StartTelegramBot(input, nil, nil, dummyAi{})
+	err := channels.StartTelegramBot(input, make(chan engine.InputMessage))
 	assert.Error(t, err)
 	assert.Contains(t, err.Error(), "bot_token is empty")
 }
 
 func TestStartTelegramBot_InvalidTokenValue(t *testing.T) {
 	// This will fail because bot.New validates the token format (usually)
-	input := entities.ChannelEntity{
+	input := &entities.ChannelEntity{
 		ID:   bson.NewObjectID(),
 		Name: "Test Bot",
 		Type: entities.ChannelTypeTelegramBot,
@@ -69,6 +63,6 @@ func TestStartTelegramBot_InvalidTokenValue(t *testing.T) {
 			"bot_token": "invalid-token",
 		},
 	}
-	err := channels.StartTelegramBot(input, nil, nil, dummyAi{})
+	err := channels.StartTelegramBot(input, make(chan engine.InputMessage))
 	assert.Error(t, err)
 }
