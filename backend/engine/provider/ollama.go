b259ece30a2884431ecf5baedc01f5f5c8221f98
Iain Cambridge <iain@iain.rocks>
2026-05-30 19:37:53 +0200

    Clean up reason ollama

--- backend/engine/provider/ollama.go
+++ backend/engine/provider/ollama.go
@@ -88,32 +88,6 @@ func (oa Ollama) Process(input engine.ReasonResponse) engine.Output {
 }
 
 func (oa Ollama) Reason(input engine.Input) *engine.ReasonResponse {
-	basePrompt := `You are a personal assistant whose job is to perform actions for the user so they don't have to. 
-Here you have to reason what the latest message is and what the outcome of the task is. 
-And what task the user is wanting performed. 
-The expected outcome is what you as the personal assistant is to do after all the information is gathered.
-The expected outcome is what is done AFTER they have provided all the information needed to perform the task.
-The expected outcome NEVER includes data gathering and MUST only be the final outcome for the user to think the task is complete, it does not need to be done by the AI/LLM but done by someone processing the task by hand, the user will not be performing the task but expect that it's done for the task to be completed if the user has to do something then it's not the expected outcome, there should be no more actions required for the task to be complete.
-Only pay attention to history messages from role as user for defining the outcome of the task.
-For example the expected outcome of "I need a car for the day tomorrow" is for a car to be rented for the day for the next day, the expected outcome of I need a flight to Paris is for there to be a flight for the user to Paris, France.
-You are able to do physical tasks and your job here is to reason if this is a physical task or a provide information task. 
-Things such as booking flights, tickets, cars, etc you are to gather info and then perform the task. 
-You are to be concise and to the point and be helpful. You are to ask for as little information as possible to get a task done. 
-If it's a new task you are provide a description of the task.  
-The related history must be directly related to the task. 
-In some cases more information is to be requested to be able to fulfill the task.
-With the following json body {history: [{role: "user", content: "message", timestamp: "2006-01-02T15:04:05Z07:00", task_id: "id"}], latest: "current_message", timestamp: "2006-01-02T15:04:05Z07:00"}.
-The response should be raw json no formatting such as new lines or escapes.
-The response must say if it's a new task, an existing task, or a generate request. And if it's a resumed task the history that is related to the task must be returned in the history. Otherwise, the history is to be empty.
-For the it to be a resumed task the latest message must be related to the the history chat in subject.
-If the task is new then don't return a task id otherwise use the task id for the related history messages. 
-The history of messages must be from the input history, YOU MUST NOT create fake history at all. 
-All the related history messages must have the same task id.
-The expected outcome is always the user gets what they want. If there is information needed to be able to do that then it MUST be in the required information. A task may take lots of chat messages to be ready.
-A task is something that is meant to be done. Some tasks are that something needs to be done, these are action verbs or needs. Some are provide information and these are when they ask questions.
-The response must just be a json response with the body and no markdown {type: "resumed_task|new_task", history: [{role: "user", content: "message", task_id: "id"], latest: "latest_message", "expected_outcome": "expected_outcome", task:{"id": "id"," "type": "PERFORM_ACTION|PROVIDE_INFORMATION"," "description": "new_description", required_information: {}}}.
-The request payload is %s`
-
 	messages := generateHistory(input.History)
 	reasonRequest := ReasonRequest{
 		History:   messages,
@@ -128,17 +102,8 @@ The request payload is %s`
 		return nil
 	}
 
-	prompt := fmt.Sprintf(basePrompt, string(encodedStruct))
-
-	stream := false
-	var output string
-	generateRequest := &ollama.GenerateRequest{
-		Model:  MODEL_NAME,
-		Prompt: prompt,
-		Stream: &stream,
-		Think:  &ollama.ThinkValue{"max"},
-	}
-
+	prompt := fmt.Sprintf(engine.PromptTaskIdentification, string(encodedStruct))
+	generateRequest := newGenerateRequest(prompt)
 	jsonRequest, err := json.Marshal(generateRequest)
 
 	if err != nil {
@@ -149,6 +114,7 @@ The request payload is %s`
 
 	oa.logger.Info(fmt.Sprintf("[Ollama] json request \"%s\"\r\n", string(jsonRequest)))
 
+	var output string
 	clientErr := oa.client.Generate(context.TODO(), generateRequest, func(resp ollama.GenerateResponse) error {
 		output += resp.Response
 
@@ -172,6 +138,16 @@ The request payload is %s`
 	}
 
 	return &reasonResp
+}
+
+func newGenerateRequest(prompt string) *ollama.GenerateRequest {
+
+	return &ollama.GenerateRequest{
+		Model:  MODEL_NAME,
+		Prompt: prompt,
+		Stream: new(false),
+		Think:  &ollama.ThinkValue{"max"},
+	}
 }
 
 func generateHistory(historyEntities []entities.HistoryEntity) []ChatMessage {
